9dd157e8c263d82794841a49e8bdc395d2456beb
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-06-04 04:07:56 +0530

    chore: Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,16 +13,19 @@ module.exports = {
     'object-curly-newline': ['error', { consistent: true }],
     'import/no-extraneous-dependencies': ['off'],
     'jsx-a11y/click-events-have-key-events': ['off'],
-    'jsx-a11y/label-has-for': ['error', { required: { every: ['id'] }, allowChildren: true }],
+    'jsx-a11y/label-has-for': [
+      'error',
+      { required: { every: ['id'] }, allowChildren: true }
+    ],
     'jsx-a11y/no-noninteractive-element-interactions': ['off'],
     'jsx-a11y/no-static-element-interactions': ['off'],
-    'react/forbid-prop-types': ['error', { forbid: ['any'] }],
-    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
     'jsx-a11y/anchor-is-valid': ['off'],
     'no-underscore-dangle': ['off'],
     'arrow-parens': ['off'],
     'no-param-reassign': ['off'],
-    'no-case-declarations': ['off']
-    // 'react/require-default-props': ['off']
+    'no-case-declarations': ['off'],
+    'no-nested-ternary': ['off'],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
+    'react/forbid-prop-types': ['off'],
   }
 };
